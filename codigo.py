9d8613ac77f5cfe3f9a840d53bed67c76a6f88d8
Leonardo Chaves Rizzi <contatoleonardo124@gmail.com>
2025-01-12 09:59:35 -0300

    Versão 1.1

--- codigo.py
+++ codigo.py
@@ -1,1 +1,10 @@
 print("Estou estudando Git neste momento!")
+
+name = input("Qual é o seu nome: ")
+primeiraNota = float(input("Digite a primeira nota da prova: "))
+segundaNota = float(input("Digite a segunda nota da prova: "))
+terceiraNota = float(input("Digite a terceira nota da prova: "))
+
+media = (primeiraNota + segundaNota + terceiraNota) / 3
+
+print(f"{name} suas notas foram\nNota 1: {primeiraNota}\nNota 2: {segundaNota}\nNota 3: {terceiraNota}\nMédia: {media:.2f}")

b08714c19e975b8903feecc45d4692c3c94e7282
Leonardo Chaves Rizzi <contatoleonardo124@gmail.com>
2025-01-12 10:18:01 -0300

    adicionando sistemas notas

--- codigo.py
+++ codigo.py
@@ -1,5 +1,3 @@
-print("Estou estudando Git neste momento!")
-
 name = input("Qual é o seu nome: ")
 primeiraNota = float(input("Digite a primeira nota da prova: "))
 segundaNota = float(input("Digite a segunda nota da prova: "))
@@ -7,4 +5,10 @@ terceiraNota = float(input("Digite a terceira nota da prova: "))
 
 media = (primeiraNota + segundaNota + terceiraNota) / 3
 
-print(f"{name} suas notas foram\nNota 1: {primeiraNota}\nNota 2: {segundaNota}\nNota 3: {terceiraNota}\nMédia: {media:.2f}")
+if media >= 6:
+    situacao = "aprovado"
+
+else:
+    situacao = "reprovado"
+
+print(f"{name} suas notas foram\nNota 1: {primeiraNota}\nNota 2: {segundaNota}\nNota 3: {terceiraNota}\nMédia: {media:.2f}\nDe acordo com sua média, você está {situacao}!")
